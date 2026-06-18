d20e697939f7ef1c65b464311e2fb473303fec54
Kirill <angelplanet333@gmail.com>
2026-06-18 19:59:32 +0300

    Добавление функции для ввода чисел

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    def get_numbers():
+        a = float(input("Введите первое число: "))
+        b = float(input("Введите второе число: "))
+        return a, b
+
+    def main():
+        print("Простой калькулятор")
+        a, b = get_numbers()
+        print(f"Вы ввели: {a} и {b}")
+
+    if __name__ == "__main__":
+        main()
